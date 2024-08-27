ed488e5bd94f2e77357ff3cf2b98a1bc25f40fc2
ScriptnSam <oluwafemisam40@gmail.com>
2024-08-27 23:22:19 +0100

    Completed authorization based routing for patients

--- scripts/main.js
+++ scripts/main.js
@@ -17,11 +17,14 @@ async function handlePatientLogin(e) {
   try {
 
     await axios.post(`${BASE_URL}/api/auth/patient/signin`, requestBody).then((res) => {
+
       const responseData = res.data;
 
       if (!responseData.error) {
-        // save token
-        localStorage.setItem('patientToken', responseData.data.token)
+
+        // Set token as cookie with expiration
+        document.cookie = `patientToken=${responseData.data.token}; expires=${new Date(Date.now() + 3600000).toUTCString()}; path=/; secure=true; sameSite=strict`;
+
 
         feedbackBox.innerHTML = `<p class="bg-green-600 rounded-sm font-semibold text-gray-100 px-4 py-2 text-lg" >${responseData.message}</p>`
         feedbackBox.classList.replace('hidden', 'block')
@@ -47,9 +50,20 @@ async function handlePatientLogin(e) {
 
   } catch (error) {
     console.error(error.message)
-    feedbackBox.innerHTML = `<p class=" bg-red-600 font-semibold text-gray-100 px-4 py-2 text-lg" >${error.message}</p>`
+    feedbackBox.innerHTML = `<p class=" bg-red-600 font-semibold text-gray-100 px-4 py-2 text-lg" >Network Error</p>`
+    // feedbackBox.innerHTML = `<p class=" bg-red-600 font-semibold text-gray-100 px-4 py-2 text-lg" >${error.message}</p>`
     feedbackBox.classList.replace('hidden', 'block')
     loginbtn.innerText = "Login"
     return;
   }
+}
+
+function patientLogout() {
+  deleteCookie('patientToken')
+  // reload page
+  window.location.reload();
+}
+
+function deleteCookie(name, path = "/") {
+  document.cookie = encodeURIComponent(name) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=" + path;
 }
--- scripts/sessionCheck.js
+++ scripts/sessionCheck.js
@@ -1,6 +1,25 @@
+const token = getCookie('patientToken');
 const checkPatient = () => {
-  const token = localStorage.getItem('patientToken');
   if (!token) {
     window.location.href = './patients.html'
   }
+}
+
+const loginAuthCheck = () => {
+  if (token) {
+    window.location.href = './profile.html'
+  }
+}
+
+function getCookie(name) {
+  const nameEQ = encodeURIComponent(name) + "=";
+  const cookiesArray = document.cookie.split('; '); // Split cookies into an array
+
+  for (let cookie of cookiesArray) {
+    if (cookie.startsWith(nameEQ)) {
+      return decodeURIComponent(cookie.substring(nameEQ.length));
+    }
+  }
+
+  return null; // Return null if cookie is not found
 }
